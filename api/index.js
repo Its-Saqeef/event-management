30a694c72a9565fd86d2eb8ef24e1ed823332733
Muhammad Saqeef <muhammadxaqeef@gmail.com>
2025-08-24 23:57:40 +0500

    added api folder

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv'
 
-import ConnectDb from './database/index.js'
-import app from './App.js'
+import ConnectDb from '../src/database/index.js'
+import app from '../src/App.js'
 
 dotenv.config()
 
